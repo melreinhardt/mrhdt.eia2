86021cb63ae9f91afb7b5600e13cfaecafedaaa2
melreinhardt <48826848+melreinhardt@users.noreply.github.com>
2020-02-10 12:37:18 +0100

    new try

--- Vogelhaus/Canvas.js
+++ Vogelhaus/Canvas.js
@@ -294,7 +294,7 @@ var Moorhuhn;
         let name = prompt("Your Score " + score, "Please enter your name"); //dann beides in Datenbank! und wenn es ausgefüllt wurde zurück zur startseite!!
         if (name != null) {
             handleSendHS(name, score);
-            window.open("https://melreinhardt.github.io/mrhdt.eia2/Vogelhaus/startingPage.html", "_self"); //self das es 
+            //window.open("https://melreinhardt.github.io/mrhdt.eia2/Vogelhaus/startingPage.html", "_self"); //self das es 
         }
     }
 })(Moorhuhn || (Moorhuhn = {}));
