8f7e2dd9ad627ebc4dd99191a13654845f62d85d
melreinhardt <48826848+melreinhardt@users.noreply.github.com>
2020-02-11 18:30:41 +0100

    pusg

--- Vogelhaus/Highscore.js
+++ Vogelhaus/Highscore.js
@@ -6,28 +6,27 @@ var Moorhuhn;
     function handleLoad(_event) {
         document.getElementById("Highscoreliste").addEventListener("click", handleRetriveHS);
     }
-    /*interface Highscore {
-        spieler: string;
-        score: string;
-    }*/
     async function handleRetriveHS(_event) {
         let query = "command=retrieve";
-        let response = await fetch(query);
+        let response = await fetch(url + "?" + query);
         let responseText = await response.text(); // das im letzten
-        //let finalResponse: any[] = JSON.parse(responseText); //
+        let finalResponse = JSON.parse(responseText); //any?
         let highscorelists = document.querySelector("div#report");
-        highscorelists.innerText = responseText;
+        //let nameArray: string[] = [];
+        //let scoreArray: string[] = [];
+        //let final: Highscore[] | null = [];
+        //for (let i: number = 0; i < finalResponse.length; i++) {
+        //  let entry: Highscore = { spieler: finalResponse[i].name, score: finalResponse[i].score };
+        //final.push(entry);
+        //let nme: string = finalResponse[i].name;
+        //let nbr: string = finalResponse[i].score;
+        //nameArray.push(nme);
+        //scoreArray.push(nbr);
+        //nameArray.push(nme);
+        //scoreArray.push(nbr);
+        highscorelists.innerText = finalResponse;
     }
 })(Moorhuhn || (Moorhuhn = {}));
-/*for (let i: number = 0; i < 10; i++) {
-    console.log(finalResponse.length);
-    finalResponse.sort();
-    let place: number = 1 + i;
-    let output: string = place + ". " + finalResponse[i].spieler + " | Score:" + finalResponse[i].score + "<br>";
-    highscorelists.innerText = output;
-}*/
-//let nameArray: string[] = [];
-//let scoreArray: string[] = [];
 //let final: Highscore[] = [];
 /*for (let i: number = 0; i < finalResponse.length; i++) {
     let entry: Highscore = { spieler: finalResponse[i].name, score: finalResponse[i].score };
@@ -39,38 +38,5 @@ var Moorhuhn;
         else
             final.push(entry);
 
-        //let nme: string = finalResponse[i].name;
-        //let nbr: string = finalResponse[i].score;
-        //nameArray.push(nme);
-        //scoreArray.push(nbr);
-    }
-    //console.log(nameArray);
-    //console.log(scoreArray);
-
-}
-
-for (let m: number = 0; m < final.length; m++) {
-    let elem: HTMLParagraphElement = document.createElement("p");
-    highscorelists.appendChild(elem);
-    elem.innerText = final[m].score + "  " + final[m].spieler;
-}
-
-//highscorelists.innerText = final; //
-//console.log(final);
-
-*/
-/*function playerDataSort(_a: Highscore, _b: Highscore): number {
- let returnNumber: number;
- if (_b.score > _a.score) {
-     returnNumber = -1;
- }
- else if (_b.score < _a.score) {
-     returnNumber = 1;
- }
- else {
-     returnNumber = 0;
- }
- return returnNumber;
-
-}*/
+ 
 //# sourceMappingURL=Highscore.js.map
--- Vogelhaus/Highscore.ts
+++ Vogelhaus/Highscore.ts
@@ -8,45 +8,43 @@ namespace Moorhuhn {
 
     }
 
-    /*interface Highscore {
+    interface Highscore {
         spieler: string;
         score: string;
-    }*/
+    }
 
 
 
     async function handleRetriveHS(_event: Event): Promise<void> {
         let query: string = "command=retrieve";
-        let response: Response = await fetch(query);
+        let response: Response = await fetch(url + "?" + query);
         let responseText: string = await response.text();   // das im letzten
-        //let finalResponse: any[] = JSON.parse(responseText); //
+        let finalResponse: any[] = JSON.parse(responseText); //any?
 
         let highscorelists: HTMLDivElement = <HTMLDivElement>document.querySelector("div#report");
 
-        
-        highscorelists.innerText = responseText;
+        //let nameArray: string[] = [];
+        //let scoreArray: string[] = [];
+
+        //let final: Highscore[] | null = [];
+
+        //for (let i: number = 0; i < finalResponse.length; i++) {
+        //  let entry: Highscore = { spieler: finalResponse[i].name, score: finalResponse[i].score };
+        //final.push(entry);
+        //let nme: string = finalResponse[i].name;
+        //let nbr: string = finalResponse[i].score;
+        //nameArray.push(nme);
+        //scoreArray.push(nbr);
+
+        //nameArray.push(nme);
+        //scoreArray.push(nbr);
+
+
+        highscorelists.innerText = finalResponse;
 
 
     }
 }
-
-
-
-
-
-
-/*for (let i: number = 0; i < 10; i++) {
-    console.log(finalResponse.length);
-    finalResponse.sort();
-    let place: number = 1 + i;
-    let output: string = place + ". " + finalResponse[i].spieler + " | Score:" + finalResponse[i].score + "<br>";
-    highscorelists.innerText = output;
-}*/
-
-
-
-        //let nameArray: string[] = [];
-        //let scoreArray: string[] = [];
 
 
 
@@ -63,37 +61,3 @@ namespace Moorhuhn {
         else
             final.push(entry);
 
-        //let nme: string = finalResponse[i].name;
-        //let nbr: string = finalResponse[i].score;
-        //nameArray.push(nme);
-        //scoreArray.push(nbr);
-    }
-    //console.log(nameArray);
-    //console.log(scoreArray);
-
-}
-
-for (let m: number = 0; m < final.length; m++) {
-    let elem: HTMLParagraphElement = document.createElement("p");
-    highscorelists.appendChild(elem);
-    elem.innerText = final[m].score + "  " + final[m].spieler;
-}
-
-//highscorelists.innerText = final; //
-//console.log(final);
-
-*/
-/*function playerDataSort(_a: Highscore, _b: Highscore): number {
- let returnNumber: number;
- if (_b.score > _a.score) {
-     returnNumber = -1;
- }
- else if (_b.score < _a.score) {
-     returnNumber = 1;
- }
- else {
-     returnNumber = 0;
- }
- return returnNumber;
-
-}*/
